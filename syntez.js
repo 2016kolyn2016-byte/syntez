f32a9e7974a36ff2f9d7c3ee1dd2f91fa54c0dd5
2016kolyn2016-byte <2016kolyn2016@gmail.com>
2026-09-15 22:42:11 +0300

    Refactor syntez.js to improve structure and clarity

--- syntez.js
+++ syntez.js
@@ -1,297 +1,202 @@
-var syntez = (function () {
-    var gen = 0, wtr = null;
-    var activeSrc = null;
+var syn = (function() {
+    var wtr = null, ind = 0;
+    function free(deleting) {
+        var fn = this;
+        if (!fn || !fn.srcs) return;
 
-    function supply(src) {
-        if (activeSrc === src) return
-        var oldActive = activeSrc;
-        activeSrc = src;
-
-        var wts = src.wts;
-        if (src.wts && src.wts.length) {
-            delete src.wts;
-            var i = wts.length, wtrLocal;
-            while(i--) {
-                wtrLocal = wts[i];
-                wtrLocal(src)
-            }
-            for (i = wts.length - 1; i >= 0; i--) {
-                if (wts[i].gen !== gen) {
-                    var d = wts[i].dps, j = d.length;
-                    while(j--) if (d[j] === src) {
-                        d[j] = d[d.length - 1];
-                        d.length--;
-                        break
-                    }
-                    wts[i] = wts[wts.length - 1];
-                    wts.length--
-                }
-            }
-            src.wts = wts
+        // 1. Отписываем эффект из всех источников
+        for (var i = 0; i < fn.srcs.length; i++) {
+            var wtrs = fn.srcs[i].wtrs;
+            var idx = wtrs.indexOf(fn);
+            if (idx !== -1) wtrs.splice(idx, 1);
         }
-        activeSrc = oldActive
-    }
+        fn.srcs.length = 0;
 
-
-    function syntez(data) {
-        for (var k in data) if (Object.hasOwnProperty.call(data, k)) switch(k) {
-            case'console':
-                consoleSet(data.console, data);
-                break;
-            case'html': {
-                var h = data.html;
-                for (var k in h) if (Object.hasOwnProperty.call(h, k)) switch(k) {
-                    case'head': {
-                        document.head.textContent = '';
-                        document.head.appendChild(html(h.head, document.head, 'meta'));
-                        break
-                    }
-                    case'body': {
-                        document.body.textContent = '';
-                        document.body.appendChild(html(h.body, document.body, 'main'));
-                        break
-                    }
-                    default: htmlAttr(document.documentElement, k, h[k])
-                }
-            } break
+        // 2. Если нужно полностью удалить реактив (сигнал/эффект)
+        if (deleting) {
+            if (fn.wtrs) fn.wtrs.length = 0;
+            fn.val = undefined;
+            fn.ind = 0;
         }
     }
-
-
-    syntez.keys = function() {
-        var k = { Control: false, Shift: false, Alt: false, Meta: false};
-        function keys() {
-            var wts = keys.wts, i = wts ? wts.length : 0;
-            if (wts) while(i--) if (wts[i] === wtr) break;
-            if (wtr && i < 0) {
-                wts[wts.length] = wtr;
-                wtr.dps[wtr.dps.length] = keys
-            }
-            return k
-        }
-        keys.wts = [];
-        window.onkeydown = function(e) {
-            k = { Control: e.ctrlKey, Shift: e.shiftKey, Alt: e.altKey, Meta: e.metaKey };
-            k[e.key] = e.code;
-            supply(keys)
-        };
-        window.onkeyup = function(e) {
-            k = { Control: e.ctrlKey, Shift: e.shiftKey, Alt: e.altKey, Meta: e.metaKey };
-            supply(keys)
-        };
-        window.onblur = function() {
-            k = { Control: false, Shift: false, Alt: false, Meta: false};
-            supply(keys)
-        };
-        return(syntez.keys = keys)()
-    };
-
-    syntez.var = function(def) {
-        function VAR(data) {
+    function tez(value) {
+        function tez(value) {
             if (arguments.length) {
-                if (VAR.val === data) return VAR.val
-                VAR.val = data;
-                supply(VAR)
-            } else {
-                var wts = VAR.wts, i = wts ? wts.length : 0;
-                if (wts) while(i--) if (wts[i] === wtr) break;
-                if (wtr && i < 0) {
-                    wts[wts.length] = wtr;
-                    wtr.dps[wtr.dps.length] = VAR
+                var val = tez.val;
+                tez.val = value;
+                var wtrs = tez.wtrs.slice();
+                for (var i = 0; i < wtrs.length; i++) {
+                    try { syn.tez(wtrs[i]) } catch(e) { console.error(e) }
                 }
-                if (!Object.hasOwnProperty.call(VAR, 'val')) {
-                    if (def instanceof Function) {
-                        var oldWtr = wtr;
-                        wtr = VAR;
-                        VAR.gen = gen;
-                        try {
-                            VAR.val = def()
-                        }
-                        finally { wtr = oldWtr }
-                    } else VAR.val = def
+                return val
+            } else if (wtr) {
+                if (tez.wtrs.indexOf(wtr) === -1) {
+                    tez.wtrs[tez.wtrs.length] = wtr;
+                    wtr.srcs[wtr.srcs.length] = tez;
                 }
-                return VAR.val
+                tez.ind = wtr.ind
             }
-            return VAR.val
+            return tez.val
         }
-        VAR.isVar = true;
-        VAR.wts = [];
-        VAR.dps = [];
-        if (def && def.isAlt) def.target = VAR
-        return VAR
-    };
+        tez.val = value;
+        tez.wtrs = [];
+        tez.ind = 0;
+        tez.free = free;
+        return tez
+    }
+    syn = tez({});
+    syn.tez = function() {
+        if (arguments.length) {
+            var arg = arguments[0];
+            if (arg instanceof Function) {
+                if (!arg.srcs) arg.srcs = [];
+                arg.ind = ++ind;
+                wtr = arg;
+                try { arg() } catch(e) { console.error(e) } finally { wtr = null }
 
-    syntez.alt = function Alt() {
-        if (arguments.length < 2) return new Error()
-        var val;
-        var args = Array.prototype.slice.call(arguments);
-        function alt(source) {
-            if (alt.computing) return val
-            
-            if (alt.vals) {
-                alt.computing = true;
-                var oldWtr = wtr;
-                wtr = alt;
-                alt.gen = gen;
-                try {
-                    for (var i = 0, vals = alt.vals, l = vals.length; i < l; i++) {
-                        if (vals[i] instanceof Function) {
-                            var t = vals[i]();
-                            if (t || t === 0) { val = t; break }
-                        } else {
-                            val = vals[i];
-                            break
-                        }
+                // Очистка отпавших зависимостей
+                for (var i = 0, j = 0; i < arg.srcs.length; i++) {
+                    var src = arg.srcs[i];
+                    if (src.ind === arg.ind) {
+                        arg.srcs[j++] = src; // Зависимость вызывалась в этом запуске
+                    } else {
+                        var idx = src.wtrs.indexOf(arg);
+                        if (idx !== -1) src.wtrs.splice(idx, 1); // Не вызывалась — отписываем
                     }
-                } finally {
-                    wtr = oldWtr;
-                    alt.computing = false
                 }
-                delete alt.vals
-            }
-            if (source) {
-                alt.computing = true;
-                var oldWtr = wtr;
-                wtr = alt;
-                try {
-                    for (var i = 0; i < args.length; i++) {
-                        if (args[i] instanceof Function) {
-                            var t = args[i]();
-                            if (t || t === 0) { val = t; break }
-                        }
-                    }
-                } finally {
-                    wtr = oldWtr;
-                    alt.computing = false
-                }
-                if (alt.target) alt.target.val = val;
-                supply(alt)
-            }
-            var wts = alt.wts, i = wts ? wts.length : 0;
-            if (wts) while(i--) if (wts[i] === wtr) break;
-            if (wtr && i < 0) {
-                wts[wts.length] = wtr;
-                wtr.dps[wtr.dps.length] = alt
-            }
-            return val
-        }
-        alt.isAlt = true;
-        alt.computing = false;
-        alt.wts = [];
-        alt.dps = [];
-        alt.vals = args;
-        return alt
+                arg.srcs.length = j;
+            } else return tez(arg)
+        } else return tez()
     };
+    return syn
+})();
 
 
-    function consoleSet(data, ctx) {
-        if (data instanceof Function) {
-            function set() {
-                var oldWtr = wtr;
-                wtr = set;
-                set.gen = gen;
-                try { consoleSet(data.call(ctx), ctx) }
-                finally { wtr = oldWtr }
+syn.keys = function(key) {
+    var data = {}, act = syn.tez(data);
+    syn.keys = function() { return act() };
+    window.onkeydown = window.onkeyup = window.onblur = function(e) {
+        if (!e.ctrlKey) delete data.Control;
+        if (!e.shiftKey) delete data.Shift;
+        if (!e.altKey) delete data.Alt;
+        if (!e.metaKey) delete data.Meta;
+        if (e.type === 'keydown') data[e.key] = e.code;
+        else if (e.type === 'keyup') delete data[e.key];
+        else data = {};
+        act(data)
+    };
+    return arguments.length ? syn.keys(key) : syn.keys()
+};
+
+syn.mouse = function(key) {
+    var data = {}, act = syn.tez(data);
+    syn.mouse = function() { return act() };
+    window.onmousedown = function(e) {
+        data.take = e.target;
+        act(data)
+    };
+    window.onmouseup = function(e) {
+        delete data.take;
+        act(data)
+    };
+    window.onmousemove = function(e) {
+        data.x = e.offsetX;
+        data.y = e.offsetY;
+        act(data)
+    };
+    return arguments.length ? syn.mouse(key) : syn.mouse()
+};
+
+syn.location = function(key) {
+    var data = window.location + '', act = syn.tez(data);
+    syn.location = function() { return act() };
+    window.onhashchange = function(e) {
+        data = window.location + '';
+        act(data)
+    };
+    return arguments.length ? syn.location(key) : syn.location()
+};
+
+
+// View
+syn.tez((function() {
+    var props = {
+        '': function(node, data, key) {
+            console.error('Design: ' + key + ' = ' + data);
+        },
+        a: function a(node, data) {
+            node.style.boxShadow = data + 'px ' + data + 'px ' + data + 'px ' + data + 'px #000000';
+        },
+        b: function b(node, data) {
+            node.style.backgroundColor = '#' + data.toString(16).padStart(8, '0');
+        },
+        c: function c(node, data) {
+            node.style.color = '#' + data.toString(16).padStart(8, '0');
+        },
+        f: function f(node, data) {
+            node.style.fontFamily = data;
+        },
+        g: function g(node, data) {
+            var fract = data % 1, numb = data - fract;
+            node.style.fontSize = numb + 'px';
+            node.style.fontWeight = Math.round((fract || 1) * 100) * 10 - 100
+        },
+        p: function p(node, data) {
+            node.style.padding = data + 'px';
+        },
+        pt: function pt(node, data) {
+            node.style.paddingTop = data + 'px';
+        },
+        pb: function pb(node, data) {
+            node.style.paddingBottom = data + 'px';
+        },
+        pl: function pl(node, data) {
+            node.style.paddingLeft = data + 'px';
+        },
+        pr: function pr(node, data) {
+            node.style.paddingRight = data + 'px';
+        },
+        s: function s(node, data) {
+            node.style.textAlign = data === -1 ? 'left' : data === 1 ? 'right' : data === 0 ? 'center' : 'justify';
+        },
+        w: function w(node, data) {
+            console.error('Widget = ' + data);
+        },
+        x: function x(node, data) {
+            var fract = data % 1, numb = data - fract;
+            if (numb) node.style.maxWidth = numb + 'px';
+            node.style.width = (fract || 1) * 100 + '%'
+        },
+        y: function y(node, data) {
+            node.setAttribute('data-v', '');
+            var fract = data % 1, numb = data - fract;
+            if (numb) node.style.minHeight = numb + 'px';
+            if (fract || !numb) node.style.height = (fract || 1) * 100 + '%'
+        },
+        z: function z(node, data) {
+            console.error('Z = ' + data);
+        }
+    };
+    function view(node, data) {
+        node.textContent = '';
+        if (data instanceof Function) console.error('Reactive data is not supported yet');
+        else if (typeof data === 'number') node.appendChild(document.createElement('r-num')).textContent = data;
+        else if (typeof data === 'string') node.appendChild(document.createElement('span')).textContent = data;
+        else if (data instanceof Array) {
+            for (var i = 0, t; i < data.length; i++) {
+                t = data[i];
+                if (t && t.constructor === Object) for (var key in t) { if (t.hasOwnProperty(key)) (props[key] || props[''])(node, t[key], key) }
+                else if (t instanceof Array) {
+                    if (node.childNodes.length) node.appendChild(document.createElement('s-g')).textContent = ' ';
+                    view(node.appendChild(document.createElement('div')), t);
+                } else view(node, t)
             }
-            set.dps = [];
-            set()
-        } else if (data !== undefined) {
-            console.clear();
-            console[data instanceof Error ? 'error' : 'dir'](data)
+        } else if (data !== null && data !== undefined) for (var key in data) if (data.hasOwnProperty(key)) {
+            if (key) for (var key in t) { if (t.hasOwnProperty(key)) (props[key] || props[''])(node, t[key], key) }
+            else view(node, data[key])
         }
     }
-
-    function htmlAttr(el, key, val) {
-        if (val instanceof Function) {
-            function htmlAttrUp() {
-                var oldWtr = wtr;
-                wtr = htmlAttrUp;
-                htmlAttrUp.gen = gen;
-                try { htmlAttr(el, key, val()) }
-                finally { wtr = oldWtr }
-            }
-            htmlAttrUp.dps = [];
-            htmlAttrUp()
-        } else {
-            el.setAttribute(key, val)
-        }
+    return function go() {
+        if (document.body) { document.body.textContent = ''; view(document.body.appendChild(document.createElement('div')), syn().view) } else setTimeout(go, 100)
     }
-
-    function htmlSrc(el, val) {
-        if (val instanceof Function) {
-            function htmlSrcUp() {
-                var oldWtr = wtr;
-                wtr = htmlSrcUp;
-                htmlSrcUp.gen = gen;
-                try { htmlSrc(el, val()) }
-                finally { wtr = oldWtr }
-            }
-            htmlSrcUp.dps = [];
-            htmlSrcUp()
-        } else {
-            el.src = val
-        }
-    }
-
-    function htmlValue(el, val) {
-        if (val instanceof Function) {
-            var up = true;
-            function htmlValueUp() {
-                if (up) {
-                    var oldWtr = wtr;
-                    wtr = htmlValueUp;
-                    htmlValueUp.gen = gen;
-                    try { htmlValue(el, val()) }
-                    finally { wtr = oldWtr }
-                }
-            }
-            htmlValueUp.dps = [];
-            htmlValueUp();
-            if (val.isVar) el.oninput = function() {
-                up = false;
-                var v = this.value;
-                if (typeof val() === 'number' || !isNaN(v) && v !== '') v = parseFloat(v) || 0;
-                val(v);
-                up = true
-            }
-        } else {
-            el.value = val || val === 0 ? val : ''
-        }
-    }
-
-    function html(data, $, tag) {
-        if (data instanceof Function) {
-            function htmlUp() {
-                var oldWtr = wtr;
-                wtr = htmlUp;
-                htmlUp.gen = gen;
-                try {
-                    $.textContent = '';
-                    return $.appendChild(html(data(), $, tag))
-                } finally { wtr = oldWtr }
-            }
-            htmlUp.dps = [];
-            return htmlUp()
-        } else if (data instanceof Array) {
-            var el = document.createDocumentFragment();
-            for (var i = 0, l = data.length; i < l; i++) el.appendChild(html(data[i], $, tag));
-            return el
-        } else if (data instanceof Object) {
-            var el = document.createElement(tag = data.tag || tag || 'div');
-            switch(tag) { case'figure': tag = 'figcaption'; break; case'select': tag = 'option'; break; case'table': tag = 'tr'; break; case'thead': tag = 'th'; break; case'tbody': tag = 'tr'; break; case'html': tag = 'body'; break; case'body': tag = 'main'; break; case'nav': tag = 'a'; break; case'th': tag = 'td'; break; case'tr': tag = 'td'; break; case'ul': tag = 'li'; break; case'ol': tag = 'li'; break; case'main':case'header':case'footer':case'section':case'article':case'aside': tag = 'div'; break }
-            for(var k in data) if (Object.hasOwnProperty.call(data, k)) switch(k) {
-                case'tag': break;
-                case'val': {
-                    if ('value' in el) htmlValue(el, data.val);
-                    else if ('src' in el) htmlSrc(el, data.val);
-                    else el.appendChild(html(data.val, el, tag));
-                    break
-                }
-                default: htmlAttr(el, k, data[k])
-            }
-            return el
-        } else return document.createTextNode(data)
-    }
-
-    return syntez
-})()
+})());
