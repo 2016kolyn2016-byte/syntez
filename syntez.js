00aaa37a0f791b917373301aa87e8e15d256d666
2016kolyn2016-byte <2016kolyn2016@gmail.com>
2026-09-20 21:07:08 +0300

    Refactor syntez.js for improved structure and clarity

--- syntez.js
+++ syntez.js
@@ -1,10 +1,10 @@
+// Reactivity
 var syn = (function() {
     var wtr = null, ind = 0;
     function free(deleting) {
         var fn = this;
         if (!fn || !fn.srcs) return;
 
-        // 1. Отписываем эффект из всех источников
         for (var i = 0; i < fn.srcs.length; i++) {
             var wtrs = fn.srcs[i].wtrs;
             var idx = wtrs.indexOf(fn);
@@ -12,7 +12,6 @@ var syn = (function() {
         }
         fn.srcs.length = 0;
 
-        // 2. Если нужно полностью удалить реактив (сигнал/эффект)
         if (deleting) {
             if (fn.wtrs) fn.wtrs.length = 0;
             fn.val = undefined;
@@ -54,14 +53,13 @@ var syn = (function() {
                 wtr = arg;
                 try { arg() } catch(e) { console.error(e) } finally { wtr = null }
 
-                // Очистка отпавших зависимостей
                 for (var i = 0, j = 0; i < arg.srcs.length; i++) {
                     var src = arg.srcs[i];
                     if (src.ind === arg.ind) {
-                        arg.srcs[j++] = src; // Зависимость вызывалась в этом запуске
+                        arg.srcs[j++] = src
                     } else {
                         var idx = src.wtrs.indexOf(arg);
-                        if (idx !== -1) src.wtrs.splice(idx, 1); // Не вызывалась — отписываем
+                        if (idx !== -1) src.wtrs.splice(idx, 1)
                     }
                 }
                 arg.srcs.length = j;
@@ -71,116 +69,154 @@ var syn = (function() {
     return syn
 })();
 
-
-syn.keys = function(key) {
-    var data = {}, act = syn.tez(data);
-    syn.keys = function() { return act() };
-    window.onkeydown = window.onkeyup = window.onblur = function(e) {
-        if (!e.ctrlKey) delete data.Control;
-        if (!e.shiftKey) delete data.Shift;
-        if (!e.altKey) delete data.Alt;
-        if (!e.metaKey) delete data.Meta;
-        if (e.type === 'keydown') data[e.key] = e.code;
-        else if (e.type === 'keyup') delete data[e.key];
-        else data = {};
-        act(data)
+// Inputs
+syn.keys = function keys() {
+    if (!keys.data) {
+        keys.act = syn.tez(keys.data = {});
+        window.onkeydown = window.onkeyup = window.onblur = function(e) {
+            if (!e.ctrlKey) delete keys.data.Control;
+            if (!e.shiftKey) delete keys.data.Shift;
+            if (!e.altKey) delete keys.data.Alt;
+            if (!e.metaKey) delete keys.data.Meta;
+            if (e.type === 'keydown') keys.data[e.key] = e.code;
+            else if (e.type === 'keyup') delete keys.data[e.key];
+            else keys.data = {};
+            keys.act(keys.data)
+        }
+    }
+    return keys.act()
+};
+syn.mouse = function mouse() {
+    if (!mouse.data) {
+        mouse.act = syn.tez(mouse.data = {});
+        window.onmousedown = function(e) {
+            mouse.data.target = e.target;
+            mouse.act(mouse.data)
+        };
+        window.onmouseup = function(e) {
+            delete mouse.data.target;
+            mouse.act(mouse.data)
+        };
+        window.onmousemove = function(e) {
+            mouse.data.x = e.offsetX;
+            mouse.data.y = e.offsetY;
+            mouse.act(mouse.data)
+        }
+    }
+    return mouse.act()
+};
+syn.location = function location() {
+    if (!location.data) {
+        location.act = syn.tez(location.data = window.location + '');
+        window.onhashchange = function(e) { location.act(location.data = window.location + '') }
+    }
+    return location.act()
+};
+syn.email = function email(val) {
+    var act = syn.tez('');
+    function email(val) {
+        if (arguments.length) return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(val) ? act(val) : new Error('Error email')
+        else return act()
     };
-    return arguments.length ? syn.keys(key) : syn.keys()
+    email.prototype = syn.tez;
+    if (arguments.length) email(val);
+    return email
 };
 
-syn.mouse = function(key) {
-    var data = {}, act = syn.tez(data);
-    syn.mouse = function() { return act() };
-    window.onmousedown = function(e) {
-        data.take = e.target;
-        act(data)
-    };
-    window.onmouseup = function(e) {
-        delete data.take;
-        act(data)
-    };
-    window.onmousemove = function(e) {
-        data.x = e.offsetX;
-        data.y = e.offsetY;
-        act(data)
-    };
-    return arguments.length ? syn.mouse(key) : syn.mouse()
-};
-
-syn.location = function(key) {
-    var data = window.location + '', act = syn.tez(data);
-    syn.location = function() { return act() };
-    window.onhashchange = function(e) {
-        data = window.location + '';
-        act(data)
-    };
-    return arguments.length ? syn.location(key) : syn.location()
-};
-
-
+// Console
+syn.tez(function() {
+    var data = syn().console;
+    if (data instanceof Function) data = data();
+    console.log(data)
+});
 // View
 syn.tez((function() {
     var props = {
         '': function(node, data, key) {
             console.error('Design: ' + key + ' = ' + data);
         },
-        a: function a(node, data) {
-            node.style.boxShadow = data + 'px ' + data + 'px ' + data + 'px ' + data + 'px #000000';
+        a: function a(node, data, key) {
+            if (data instanceof Function) syn.tez(function() { props[key](node, data()) });
+            else node.style.boxShadow = data + 'px ' + data + 'px ' + data + 'px ' + data + 'px #000000';
         },
-        b: function b(node, data) {
-            node.style.backgroundColor = '#' + data.toString(16).padStart(8, '0');
+        b: function b(node, data, key) {
+            if (data instanceof Function) syn.tez(function() { props[key](node, data()) });
+            else node.style.backgroundColor = '#' + data.toString(16).padStart(8, '0');
         },
-        c: function c(node, data) {
-            node.style.color = '#' + data.toString(16).padStart(8, '0');
+        c: function c(node, data, key) {
+            if (data instanceof Function) syn.tez(function() { props[key](node, data()) });
+            else node.style.color = '#' + data.toString(16).padStart(8, '0');
         },
-        f: function f(node, data) {
-            node.style.fontFamily = data;
+        f: function f(node, data, key) {
+            if (data instanceof Function) syn.tez(function() { props[key](node, data()) });
+            else node.style.fontFamily = data;
         },
-        g: function g(node, data) {
-            var fract = data % 1, numb = data - fract;
-            node.style.fontSize = numb + 'px';
-            node.style.fontWeight = Math.round((fract || 1) * 100) * 10 - 100
+        g: function g(node, data, key) {
+            if (data instanceof Function) syn.tez(function() { props[key](node, data()) });
+            else {
+                var fract = data % 1, numb = data - fract;
+                node.style.fontSize = numb + 'px';
+                node.style.fontWeight = Math.round((fract || 1) * 100) * 10 - 100
+            }
         },
-        p: function p(node, data) {
-            node.style.padding = data + 'px';
+        p: function p(node, data, key) {
+            if (data instanceof Function) syn.tez(function() { props[key](node, data()) });
+            else node.style.padding = data + 'px';
         },
-        pt: function pt(node, data) {
-            node.style.paddingTop = data + 'px';
+        pt: function pt(node, data, key) {
+            if (data instanceof Function) syn.tez(function() { props[key](node, data()) });
+            else node.style.paddingTop = data + 'px';
         },
-        pb: function pb(node, data) {
-            node.style.paddingBottom = data + 'px';
+        pb: function pb(node, data, key) {
+            if (data instanceof Function) syn.tez(function() { props[key](node, data()) });
+            else node.style.paddingBottom = data + 'px';
         },
-        pl: function pl(node, data) {
-            node.style.paddingLeft = data + 'px';
+        pl: function pl(node, data, key) {
+            if (data instanceof Function) syn.tez(function() { props[key](node, data()) });
+            else node.style.paddingLeft = data + 'px';
         },
-        pr: function pr(node, data) {
-            node.style.paddingRight = data + 'px';
+        pr: function pr(node, data, key) {
+            if (data instanceof Function) syn.tez(function() { props[key](node, data()) });
+            else node.style.paddingRight = data + 'px';
         },
-        s: function s(node, data) {
-            node.style.textAlign = data === -1 ? 'left' : data === 1 ? 'right' : data === 0 ? 'center' : 'justify';
+        s: function s(node, data, key) {
+            if (data instanceof Function) syn.tez(function() { props[key](node, data()) });
+            else node.style.textAlign = data === -1 ? 'left' : data === 1 ? 'right' : data === 0 ? 'center' : 'justify';
         },
-        w: function w(node, data) {
-            console.error('Widget = ' + data);
+        w: function w(node, data, key) {
+            if (data instanceof Function) syn.tez(function() { props[key](node, data()) });
+            else console.error('Widget = ' + data);
         },
-        x: function x(node, data) {
-            var fract = data % 1, numb = data - fract;
-            if (numb) node.style.maxWidth = numb + 'px';
-            node.style.width = (fract || 1) * 100 + '%'
+        x: function x(node, data, key) {
+            if (data instanceof Function) syn.tez(function() { props[key](node, data()) });
+            else {
+                var fract = data % 1, numb = data - fract;
+                if (numb) node.style.maxWidth = numb + 'px';
+                node.style.width = (fract || 1) * 100 + '%'
+            }
         },
-        y: function y(node, data) {
-            node.setAttribute('data-v', '');
-            var fract = data % 1, numb = data - fract;
-            if (numb) node.style.minHeight = numb + 'px';
-            if (fract || !numb) node.style.height = (fract || 1) * 100 + '%'
+        y: function y(node, data, key) {
+            if (data instanceof Function) syn.tez(function() { props[key](node, data()) });
+            else {
+                node.setAttribute('data-v', '');
+                var fract = data % 1, numb = data - fract;
+                if (numb) node.style.minHeight = numb + 'px';
+                if (fract || !numb) node.style.height = (fract || 1) * 100 + '%'
+            }
         },
-        z: function z(node, data) {
-            console.error('Z = ' + data);
+        z: function z(node, data, key) {
+            if (data instanceof Function) syn.tez(function() { props[key](node, data()) });
+            else console.error('Z = ' + data);
         }
     };
     function view(node, data) {
         node.textContent = '';
-        if (data instanceof Function) console.error('Reactive data is not supported yet');
-        else if (typeof data === 'number') node.appendChild(document.createElement('r-num')).textContent = data;
+        if (data instanceof Function) {
+            if (data.prototype === syn.tez) {
+                var inp = node.appendChild(document.createElement('input'));
+                inp.value = data()
+            } else syn.tez(function() { view(node, data()) })
+        } else if (typeof data === 'number') node.appendChild(document.createElement('r-num')).textContent = data;
         else if (typeof data === 'string') node.appendChild(document.createElement('span')).textContent = data;
         else if (data instanceof Array) {
             for (var i = 0, t; i < data.length; i++) {
@@ -188,7 +224,7 @@ syn.tez((function() {
                 if (t && t.constructor === Object) for (var key in t) { if (t.hasOwnProperty(key)) (props[key] || props[''])(node, t[key], key) }
                 else if (t instanceof Array) {
                     if (node.childNodes.length) node.appendChild(document.createElement('s-g')).textContent = ' ';
-                    view(node.appendChild(document.createElement('div')), t);
+                    view(node.appendChild(document.createElement('div')), t)
                 } else view(node, t)
             }
         } else if (data !== null && data !== undefined) for (var key in data) if (data.hasOwnProperty(key)) {
