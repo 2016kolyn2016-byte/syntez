45a5f2f6c5528fd1e34086fd4065a870e8f0f016
2016kolyn2016-byte <2016kolyn2016@gmail.com>
2026-09-21 11:28:29 +0300

    Restruct project

---
+++ web/index.js
@@ -0,0 +1,9 @@
+syn({
+    console: syn.keys,
+    view: [
+        { b: 0x777777ff, c: 0x00aaffff, y: 0 },
+        [{ b: 0x555555ff, c: 0x00aaffff }, 'Header'],
+        ['Body'],
+        [{ b: 0x555555ff, c: 0x00aaffff }, 'Footer']
+    ]
+})
